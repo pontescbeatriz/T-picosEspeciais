9777301b356a21d1b9f52a8f2f7f83952e53b59a
Beatriz Pontes <35998532+pontescbeatriz@users.noreply.github.com>
2019-04-10 23:00:25 -0300

    Create Capitulo3 pense.py

---
+++ Capitulo3 pense.py
@@ -0,0 +1,126 @@
+>> >  Tipo ( 42 )
+< class  ' int ' >
+>> >  int ( ' 32 ' )
+32
+>> >  int ( ' Olá ' )
+ValueError : literal inválido para  int (): Hello
+>> >  int ( 3,99999 )
+3
+>> >  int ( - 2.3 )
+- 2
+>> >  flutuador ( 32 )
+32,0
+>> >  flutuador ( ' 3,14159 ' )
+3,14159
+>> >  str ( 32 )
+' 32 '
+>> >  str ( 3,14159 )
+' 3,14159 '
+>> >  importação de matemática
+>> > matemática
+< Módulo ' matemática ' (construído - em ) >
+>> > proporção = signal_power / noise_power
+>> > decibéis =  10  * math.log10 (razão)
+>> > radianos =  0,7
+>> > altura = Math.sin (radianos)
+>> > graus =  45
+>> > radianos = graus /  180,0  * math.pi
+>> > Math.sin (radianos)
+0,707106781187
+>> > Math.sqrt ( 2 ) /  2.0
+0,707106781187
+x = math.sin (graus /  360,0  *  2  * math.pi)
+x = math.exp (math.log (x + 1 ))
+>> > minutos = horas *  60  # Correto
+>> > horas *  60  = minutos # Errado!
+SyntaxError : pode ' t atribuir ao operador
+def  print_lyrics ():
+print ( " Sou lenhador e estou bem " )
+print ( " Eu durmo a noite toda e trabalho o dia todo " )
+>> >  def  print_lyrics ():
+...  print ( " Sou lenhador e estou bem " )
+...  print (“durmo a noite toda e trabalho o dia todo ”)
+...
+>> >  impressão (print_lyrics)
+< função print_lyrics em 0x b7e99e9c >
+>> >  Tipo (print_lyrics)
+< Classe  ' função ' >
+>> > print_lyrics ()
+I ' ma lenhador, e eu ' estou bem.
+Eu durmo a noite toda e trabalho o dia todo .
+def  repeat_lyrics ():
+print_lyrics ()
+print_lyrics ()
+>> > repeat_lyrics ()
+I ' ma lenhador, e eu ' estou bem.
+Eu durmo a noite toda e trabalho o dia todo .
+I ' ma lenhador, e eu ' estou bem.
+Eu durmo a noite toda e trabalho o dia todo .
+def  print_lyrics ():
+print ( " Sou lenhador e estou bem " )
+print ( " Eu durmo a noite toda e trabalho o dia todo " )
+def  repeat_lyrics ():
+print_lyrics ()
+print_lyrics ()
+repeat_lyrics ()
+def  print_twice ( bruce ):
+imprimir (bruce)
+imprimir (bruce)
+>> > print_twice ( ' Spam ' )
+Spam
+Spam
+>> > print_twice ( 42 )
+42
+42
+>> > print_twice (math.pi)
+3,14159265359
+3,14159265359
+>> > print_twice ( ' Spam ' * 4 )
+Spam Spam Spam Spam
+Spam Spam Spam Spam
+>> > print_twice (math.cos (math.pi))
+- 1,0
+- 1,0
+>> > michael =  ' Eric, a metade de uma abelha. '
+>> > print_twice (Michael)
+Eric, a meia abelha.
+Eric, a meia abelha.
+def  cat_twice ( part1 , part2 ):
+gato = part1 + part2
+print_twice (cat)
+>> > line1 =  ' Tiddle Bing '
+>> > line2 =  ' Tiddle estrondo. '
+>> > cat_twice (Linha1, Linha2)
+Bing tiddle tiddle bang.
+Bing tiddle tiddle bang.
+>> >  print (cat)
+NameError : o nome ' cat '  não está  definido
+Traceback (último íntimo):
+Arquivo " test.py " , linha 13 , em __main__
+cat_twice (line1, line2)
+Arquivo " test.py " , linha 5 , em cat_twice
+print_twice (cat)
+Arquivo " test.py " , linha 9 , em print_twice
+imprimir (gato)
+NameError : o nome ' cat '  não está  definido
+x = math.cos (radianos)
+golden = (math.sqrt ( 5 ) +  1 ) /  2
+>> > Math.sqrt ( 5 )
+2.2360679774997898
+>> > resultado = print_twice ( ' Bing ' )
+Bing
+Bing
+
+>> >  print (resultado)
+Nenhum
+>> >  print ( tipo ( Nenhum ))
+< class  ' NoneType ' >
+>> > right_justify ( ' Monty ' )
+def  do_twice ( f ):
+f ()
+f ()
+def  print_spam ():
+
+print ( ' spam ' )
+do_twice (print_spam)
+print ( ' + ' , ' - ' )
