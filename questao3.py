443fbf1ddf51baf969f6cab9982760cadd46b2de
Beatriz Pontes <35998532+pontescbeatriz@users.noreply.github.com>
2019-04-11 07:03:27 -0300

    Create questao3.py

---
+++ questao3.py
@@ -0,0 +1,7 @@
+x =  int ( input ())
+para eu no  intervalo (x):
+    a =  float ( input ())
+    b =  float ( input ())
+    c =  float ( input ())
+    calculo = (a * 2 ) + (b * 3 ) + (c * 5 ) / 3.0
+    print ( " Média : % .1f " % (calculo))
