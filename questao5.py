7d9c72e234813f96fc8bb6cdba9e37c15d6abca6
Beatriz Pontes <35998532+pontescbeatriz@users.noreply.github.com>
2019-04-11 07:04:27 -0300

    Create questao5.py

---
+++ questao5.py
@@ -0,0 +1,7 @@
+lista =  list ()
+para eu na  faixa ( 6 ):
+    valor =  float ( input ())
+    if (valor > = 1 ):
+        l.append (valor)
+soma =  soma (l)
+print ( " Média dos valores Positivos: % .1f "  % (soma / len (l)))
