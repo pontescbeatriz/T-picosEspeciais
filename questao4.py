d71bcee5dffe924ff0fede5710e02026deef5355
Beatriz Pontes <35998532+pontescbeatriz@users.noreply.github.com>
2019-04-11 07:04:02 -0300

    Create questao4.py

---
+++ questao4.py
@@ -0,0 +1,8 @@
+lista =  list ()
+para eu na  faixa ( 15 ):
+    valor =  int ( input ())
+    l.append (valor)
+
+print ( " LISTA: " , l)
+print ( " Valor Máximo que está na lista: % d " % ( max (l)))
+print ( " Lugar onde está a maior posição da lista " , l.index ( max (l)))
