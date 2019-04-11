93995fab21937c60e83f1e33d3682df4c4932429
Beatriz Pontes <35998532+pontescbeatriz@users.noreply.github.com>
2019-04-11 07:02:49 -0300

    Create questao2.py

---
+++ questao2.py
@@ -0,0 +1,8 @@
+l1 =  list ()
+l2 =  list ()
+para eu na  faixa ( 6 ):
+    valor =  int ( input ())
+    if (valor <  0 ): l2.append (valor)
+    else : l1.append (valor)
+print ( " Valores Positivos: " , l1)
+print ( " Valores Negativos: " , l2)
